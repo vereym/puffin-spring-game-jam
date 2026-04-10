b5158c14a5b84429de8af886a5fbb78cff88e0d0
vaeng <34183939+vaeng@users.noreply.github.com>
2026-04-10 10:21:49 +0200

    add torque movement

--- main/player/player_controller.gd
+++ main/player/player_controller.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var force = 10000
+@onready var torso: RigidBody2D = $"."
 
 func _ready():
 	can_sleep = false
@@ -13,3 +14,10 @@ func _integrate_forces(state):
 
 # func _physics_process(delta: float) -> void:
 # 	$RightPinJoint2D.transform
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("q-key"):
+		torso.apply_torque(1000000)
+		print("q was pressed")
+	elif Input.is_action_just_pressed("p-key"):
+		torso.apply_torque(-1000000)
